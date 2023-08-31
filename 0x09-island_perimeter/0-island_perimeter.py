4564021c04171ba4bcc99eeb10c3ea22f4ff18ee
NuelUzoma <emcool17@gmail.com>
2023-08-31 14:19:40 +0100

    Code Fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-"""a function def island_perimeter(grid):
+"""A function def island_perimeter(grid):
 that returns the perimeter of the island described in grid"""
 
 
-def solve(matrix):
+def island_perimeter(grid):
     """The function of the program"""
     d = 0
     perimeter = 0
-    height = len(matrix)
-    length = len(matrix[0])
-    for line in matrix:
+    height = len(grid)
+    length = len(grid[0])
+    for line in grid:
         c = 0
 
         for val in line:
             if val == 1:
                 surround = 4
                 if c != length - 1:
-                    if matrix[d][c + 1] == 1:
+                    if grid[d][c + 1] == 1:
                         surround -= 1
                 if c != 0:
-                    if matrix[d][c - 1] == 1:
+                    if grid[d][c - 1] == 1:
                         surround -= 1
                 if d != height - 1:
-                    if matrix[d + 1][c] == 1:
+                    if grid[d + 1][c] == 1:
                         surround -= 1
                 if d != 0:
-                    if matrix[d - 1][c] == 1:
+                    if grid[d - 1][c] == 1:
                         surround -= 1
                 perimeter += surround
             c += 1
